7151f4b766bd4820ad878651da9735f7a265ccfc
uuuumy <e.idanova@g.nsu.ru>
2026-04-25 13:49:26 +0700

    12.20.py

--- 12.20.py
+++ 12.20.py
@@ -1,9 +1,11 @@
 mem = {}
 
+
 def lcs_recursive(stra, strb, i, j):
     """
     An auxiliary recursive function for calculating
-    the length of the longest common subsequence
+    the length of the longest common SUBSTRING (consecutive characters)
+    ENDING at positions i-1 and j-1
     """
     if (i, j) in mem:
         return mem[(i, j)]
@@ -15,19 +17,28 @@ def lcs_recursive(stra, strb, i, j):
     if stra[i - 1] == strb[j - 1]:
         result = 1 + lcs_recursive(stra, strb, i - 1, j - 1)
     else:
-        result = max(lcs_recursive(stra, strb, i - 1, j),
-                     lcs_recursive(stra, strb, i, j - 1))
+        result = 0
 
     mem[(i, j)] = result
     return result
 
+
 def comp(a, b, m, n):
     """
-    Finds the maximum length of a common
-    subsequence of characters for strings a and b.
+    Finds the maximum length of a common SUBSTRING
+    (consecutive characters) for strings a and b.
     """
     global mem
     mem = {}
-    return lcs_recursive(a, b, m, n)
 
-print(comp('abcde', 'ace', 5, 3))
+    max_length = 0
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            length = lcs_recursive(a, b, i, j)
+            if length > max_length:
+                max_length = length
+
+    return max_length
+
+
+print(comp('abxc', 'xbxc', 4, 4))
